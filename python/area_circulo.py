1bee8d166a2dfca0579eb59912e0cdc6bc6fdfdb
huntzzxx <william.william@discente.ufg.br>
2024-01-13 19:39:45 -0300

    final version

--- python/area_circulo.py
+++ python/area_circulo.py
@@ -1,11 +1,36 @@
 #! python
 
 import math
-print('π = ', math.pi)
-pi = 3.14159
-raio = input('Informe o raio: ')
+import sys
+import errno
 
-print('Área do círculo: ', math.pi * (float(raio) ** 2))
+class TerminalColor:
+    erro = '\033[91m'
+    normal = '\033[0m'
 
 
-teste = input('Teste 1, 2, 3...')
+def help():
+    print("É necessário informar o raio de um círculo.")
+    print("Sintaxe: {} <raio>".format(sys.argv[0][2:]))
+
+
+def circulo(raio):
+    return math.pi * (float(raio) ** 2)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        help()
+        sys.exit(errno.EPERM)
+    elif not sys.argv[1].isnumeric():
+        help()
+        print(TerminalColor.erro + 'O raio deve ser um valor númerico.', TerminalColor.normal)
+        sys.exit(errno.EINVAL)
+    else:
+        raio = sys.argv[1]
+        try:
+            area = circulo(raio)
+            print('Área do círculo:', area)
+        except ValueError:
+            print("Erro: Certifique-se de que o raio fornecido é um número válido.")
+            sys.exit(1)
